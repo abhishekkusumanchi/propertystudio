c23c2a5c91ae424b5c7119a4f2842bd36b5dd9e4
abhishekkusumanchi <94862160+abhishekkusumanchi@users.noreply.github.com>
2024-06-02 17:27:50 +0530

    Created tables_deletion.sql

---
+++ DBScripts/tables_deletion.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS prs_payments;
+DROP TABLE IF EXISTS prs_customer_users;
+DROP TABLE IF EXISTS prs_villa_properties;
+DROP TABLE IF EXISTS prs_commercial_complex_properties;
+DROP TABLE IF EXISTS prs_flat_properties;
+DROP TABLE IF EXISTS prs_apartment_floors;
+DROP TABLE IF EXISTS prs_project_commercial_complexes;
+DROP TABLE IF EXISTS prs_project_villas;
+DROP TABLE IF EXISTS prs_project_apartments;
+DROP TABLE IF EXISTS prs_customers;
+DROP TABLE IF EXISTS prs_site_visits;
+DROP TABLE IF EXISTS prs_enquiries;
+DROP TABLE IF EXISTS prs_properties;
+DROP TABLE IF EXISTS prs_projects;
+DROP TABLE IF EXISTS prs_employees;
+DROP TABLE IF EXISTS prs_admin_users;
+DROP TABLE IF EXISTS prs_project_types;
